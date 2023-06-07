b55c7e7041b0dedd764d126603656606c806e305
Abdoulrachard <Abdoulrachard@gmail.com>
2023-06-07 15:44:07 +0100

    Modification de la navbar

---
+++ assets/Js/animation.js
@@ -0,0 +1,12 @@
+new fJs.NumberAutoCount({
+    timeout: 500
+});
+new fJs.AutoWriteText();
+new fJs.Intersection({
+    elt: '.revealElement',
+    class: "animate__animated animate__fadeInUp opacity-100",
+    root: null,
+    ratio: 0.2,
+    rootMargin: '0px',
+    threshold: 0.9,
+});
