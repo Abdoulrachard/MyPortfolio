22f90e51b89d8c1dba274c70a2d341a47db11c09
Abdoulrachard <Abdoulrachard@gmail.com>
2023-10-03 18:26:49 +0100

    optimisation des image et autre

--- assets/Js/animation.js
+++ assets/Js/animation.js
@@ -1,18 +1,11 @@
-window.addEventListener("load", function() {
-    const preloader = document.querySelector(".preloader");
-    preloader.classList.add("preload-finish");
-  });
+// window.addEventListener("load", function() {
+//     const preloader = document.querySelector(".preloader");
+//     preloader.classList.add("preload-finish");
+//   });
 new fJs.NumberAutoCount({
     timeout: 500 
 });
 new fJs.AutoWriteText();
-new fJs.Intersection({
-    elt: '.revealElement',
-    class: "animate__animated animate__fadeInUp opacity-100",
-    root: null,
-    ratio: 0.2,
-    rootMargin: '0px',
-    threshold: 0.9,
-});
+
 
 
